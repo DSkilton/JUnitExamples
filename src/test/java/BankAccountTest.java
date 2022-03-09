bf607dda0334784ec1d0c4bddaf859e3ae9fcdbe
Duncan R Skilton <mc03353@mbro.ac.uk>
2022-03-09 12:36:52 +0000

    added testDeposit - pass

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -15,11 +15,18 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author MC03353
  */
 public class BankAccountTest {
-    
+
     @Test
-    public void testWithdraw(){
+    public void testWithdraw() {
         BankAccount bankAccount = new BankAccount(500, -1000);
         bankAccount.withdraw(300);
         assertEquals(200, bankAccount.getBalance());
+    }
+
+    @Test
+    public void testDeposit() {
+        BankAccount bankAccount = new BankAccount(400, 0);
+        bankAccount.deposit(500);
+        assertEquals(900, bankAccount.getBalance());
     }
 }
