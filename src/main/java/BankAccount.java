c12d670e07bd369418d7f079056134a27f0c90ca
Duncan R Skilton <mc03353@mbro.ac.uk>
2022-03-09 12:31:10 +0000

    added testWithdraw - pass

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -17,6 +17,33 @@ public class BankAccount {
         
     }
     
+    public double withdraw(double amount){
+        if(balance - amount > minimumBalance){
+            balance -= amount;
+            return amount;
+        } else {
+            throw new RuntimeException();
+        }
+    }
     
+    public double deposit(double amount){
+        return balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinimumBalance() {
+        return minimumBalance;
+    }
+
+    public void setMinimumBalance(double minimumBalance) {
+        this.minimumBalance = minimumBalance;
+    }
     
 }
