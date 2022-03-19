e9394d108408197f7db1df0c58c14285e9ef56fe
Duncan R Skilton <mc03353@mbro.ac.uk>
2022-03-19 08:34:15 +0000

    added test to check active state of bank account

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -9,14 +9,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
 
 /**
  *
  * @author MC03353
  */
+@DisplayName("Test BankAccount Class")
 public class BankAccountTest {
 
     @Test
+    @DisplayName("Withdraw 500 successfully")
     public void testWithdraw() {
         BankAccount bankAccount = new BankAccount(500, -1000);
         bankAccount.withdraw(300);
@@ -24,9 +27,19 @@ public class BankAccountTest {
     }
 
     @Test
+    @DisplayName("Deposit 400 successfully")
     public void testDeposit() {
         BankAccount bankAccount = new BankAccount(400, 0);
         bankAccount.deposit(500);
         assertEquals(900, bankAccount.getBalance());
+    }
+    
+    @Test
+    @DisplayName("withdrawel will will become negative")
+    public void testWithdrawNotStuckAtZero(){
+        BankAccount bankAccount = new BankAccount(500, 1000);
+        bankAccount.withdraw(800);
+        assertNotEquals(0, bankAccount.getBalance());
+        
     }
 }
