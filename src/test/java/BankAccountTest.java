c222367ea41c6e183bd8b4fd7345ff2a9cf1e114
Duncan R Skilton <mc03353@mbro.ac.uk>
2022-03-19 08:57:30 +0000

    added assumptionsTest class

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -19,6 +19,22 @@ import org.junit.jupiter.api.DisplayName;
 @DisplayName("Test BankAccount Class")
 public class BankAccountTest {
 
+    @BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+    }
+
     @Test
     @DisplayName("Withdraw 500 successfully")
     public void testWithdraw() {
@@ -77,6 +93,101 @@ public class BankAccountTest {
     public void testDepositTimeout(){
         BankAccount bankAccount = new BankAccount(400, 0);
         assertTimeout(Duration.ofNanos(1), () -> bankAccount.deposit(200));
+    }
+
+    /**
+     * Test of getBalance method, of class BankAccount.
+     */
+    @Test
+    public void testGetBalance() {
+        System.out.println("getBalance");
+        BankAccount instance = null;
+        double expResult = 0.0;
+        double result = instance.getBalance();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setBalance method, of class BankAccount.
+     */
+    @Test
+    public void testSetBalance() {
+        System.out.println("setBalance");
+        double balance = 0.0;
+        BankAccount instance = null;
+        instance.setBalance(balance);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMinimumBalance method, of class BankAccount.
+     */
+    @Test
+    public void testGetMinimumBalance() {
+        System.out.println("getMinimumBalance");
+        BankAccount instance = null;
+        double expResult = 0.0;
+        double result = instance.getMinimumBalance();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setMinimumBalance method, of class BankAccount.
+     */
+    @Test
+    public void testSetMinimumBalance() {
+        System.out.println("setMinimumBalance");
+        double minimumBalance = 0.0;
+        BankAccount instance = null;
+        instance.setMinimumBalance(minimumBalance);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isActive method, of class BankAccount.
+     */
+    @Test
+    public void testIsActive() {
+        System.out.println("isActive");
+        BankAccount instance = null;
+        boolean expResult = false;
+        boolean result = instance.isActive();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getHolderName method, of class BankAccount.
+     */
+    @Test
+    public void testGetHolderName() {
+        System.out.println("getHolderName");
+        BankAccount instance = null;
+        String expResult = "";
+        String result = instance.getHolderName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setHolderName method, of class BankAccount.
+     */
+    @Test
+    public void testSetHolderName() {
+        System.out.println("setHolderName");
+        String holderName = "";
+        BankAccount instance = null;
+        instance.setHolderName(holderName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
     
 }
