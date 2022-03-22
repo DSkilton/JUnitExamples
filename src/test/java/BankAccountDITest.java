e0fb7274e4049e69a30f4bd1774ac3e56daf0281
Duncan R Skilton <mc03353@mbro.ac.uk>
2022-03-22 20:04:30 +0000

    adding parameter resolver class

--- src/test/java/BankAccountDITest.java
+++ src/test/java/BankAccountDITest.java
@@ -1,3 +1,7 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +11,12 @@
  *
  * @author MC03353
  */
+
+@ExtendWith(BankAccountParameterResolver.class)
 public class BankAccountDITest {
     
+    public void testDeposit(BankAccount bankAccount){
+        bankAccount.deposit(500);
+        assertEquals(500, bankAccount.getBalance());
+    }
 }
--- src/test/java/BankAccountParameterResolver.java
+++ src/test/java/BankAccountParameterResolver.java
@@ -1,3 +1,9 @@
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +13,16 @@
  *
  * @author MC03353
  */
-public class BankAccountParameterResolver {
+public class BankAccountParameterResolver implements ParameterResolver {
+
+    @Override
+    public boolean supportsParameter(ParameterContext pc, ExtensionContext ec) throws ParameterResolutionException {
+        return pc.getParameter().getType() == BankAccount.class;
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext pc, ExtensionContext ec) throws ParameterResolutionException {
+        return new BankAccount(0, 0);
+    }
     
 }
