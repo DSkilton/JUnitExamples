1beafec56b48702141c1975d4d19ee7b91e40bb0
Duncan R Skilton <mc03353@mbro.ac.uk>
2022-04-20 10:15:31 +0100

    added @BeforeAll annotation

--- src/test/java/BankAccountBeforeAndAfterTest.java
+++ src/test/java/BankAccountBeforeAndAfterTest.java
@@ -1,3 +1,9 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +14,24 @@
  * @author MC03353
  */
 public class BankAccountBeforeAndAfterTest {
+    static BankAccount bankAccount;
     
+    @BeforeAll
+    public static void prepTest(){
+        bankAccount = new BankAccount(500, 0);
+    }
+    
+    @Test
+    public void testWithdraw(){
+        bankAccount.withdraw(300);
+        assertEquals(200, bankAccount.getBalance());
+    }
+    
+    @Test
+    public void testDeposit(){
+        bankAccount.deposit(500);
+        assertEquals(500, bankAccount.getBalance());
+    }
+    
+
 }
